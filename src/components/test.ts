825159a112984ab128664e245c348c573daf75fd
kai63001 <animehq5@gmail.com>
2021-12-08 14:57:55 +0700

    update

---
+++ src/components/test.ts
@@ -0,0 +1,5 @@
+const data = () => {
+    return "tesrt"
+}
+
+export default data;
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import { Client,Intents } from "discord.js";
+import test from "@components/test"
+
+console.log(test())
