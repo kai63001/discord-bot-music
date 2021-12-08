e40de2842acd11cb42674147cd2ce6422526b466
kai63001 <animehq5@gmail.com>
2021-12-08 16:39:00 +0700

    udpate env

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { Client, Intents } from "discord.js";
-const token = "OTE4MDQ2NjcwMjE2OTIxMTIw.YbBjlg.J3IaADXHb-f1nu03JRJD8b_JtTs";
+require("dotenv").config();
 
+const token = process.env.TOKEN;
 
-const clinet = new Client({intents: [Intents.FLAGS.GUILDS]})
+const clinet = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-clinet.once('ready', ()=> {
-    console.log("ready")
-})
+clinet.once("ready", () => {
+  console.log("ready");
+});
 
-
-clinet.login(token)
+clinet.login(token);
