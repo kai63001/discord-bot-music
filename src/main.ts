052f3ed5b7796c6f0595fd6d86d732356fb9d41a
kai63001 <animehq5@gmail.com>
2021-12-08 17:40:25 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,38 @@
 import { Client, Intents } from "discord.js";
 require("dotenv").config();
+import { joinVoiceChannel, getVoiceConnection } from "@discordjs/voice";
 
 const token = process.env.TOKEN;
 
-const clinet = new Client({intents: [Intents.FLAGS.GUILDS ,Intents.FLAGS.GUILD_MESSAGES]})
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+  ],
+});
 
-clinet.once("ready", () => {
+client.once("ready", () => {
   console.log("ready");
 });
 
-clinet.on('messageCreate' , msg=>{
-    if (msg.content == "ปราม"){
-        // console.log(msg)
-        msg.reply("ไอ้เด็กเหี้ย")
+client.on("messageCreate", async (msg) => {
+  if (msg.content == "ปราม") {
+    console.log(msg);
+    msg.reply("ควย นัท");
+  } else if (msg.content == "!join") {
+    if (!msg.member?.voice.channel) {
+      msg.reply("JOIN ห้อง ก่อนไอ้สัส");
+    } else {
+      const connection: any = joinVoiceChannel({
+        channelId: msg.member?.voice.channel?.id as string,
+        guildId: msg.member?.guild.id as string,
+        // @ts-ignore: Unreachable code error
+        adapterCreator: msg.channel.guild.voiceAdapterCreator,
+      });
     }
-})
+  }
+});
 
-clinet.login(token)
+client.login(token);
