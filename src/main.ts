a4278ecb08d326b6332330059e3f85fa3d0151fb
kai63001 <animehq5@gmail.com>
2021-12-08 16:34:36 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
-import { Client,Intents } from "discord.js";
-import test from "@components/test"
+import { Client, Intents } from "discord.js";
+const token = "OTE4MDQ2NjcwMjE2OTIxMTIw.YbBjlg.J3IaADXHb-f1nu03JRJD8b_JtTs";
 
-console.log(test())
+
+const clinet = new Client({intents: [Intents.FLAGS.GUILDS]})
+
+clinet.once('ready', ()=> {
+    console.log("ready")
+})
+
+
+clinet.login(token)
