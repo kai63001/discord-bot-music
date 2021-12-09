152834658bedba2230174e38ff4787e081a9bff5
kai63001 <animehq5@gmail.com>
2021-12-09 16:47:00 +0700

    update 😊

--- src/components/youtube.ts
+++ src/components/youtube.ts
@@ -1,9 +1,7 @@
 import ytdl from "ytdl-core";
 
 const youtube = async (msg: any) => {
-  const link = msg.content
-    .replace("!play https://www.youtube.com/watch?v=", "")
-    .trim();
+  let link = msg.content.replace("!play ", "").trim();
   let info = await ytdl.getInfo(link);
   let format = ytdl.filterFormats(info.formats, "audioonly");
   return format;
